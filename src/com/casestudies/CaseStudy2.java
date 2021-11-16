e3762a0ade3033f525ff30c89e7da9685d951620
whalewalker <mongodbms@gmail.com>
2021-11-16 22:33:24 +0100

    Implement case study 2

--- src/com/casestudies/CaseStudy2.java
+++ src/com/casestudies/CaseStudy2.java
@@ -1,4 +1,19 @@
 package com.casestudies;
 
 public class CaseStudy2 {
+
+    public static double calculateTicket(int ticketNumber, Category category){
+        double total;
+        if(category == Category.SILVER){
+            total = ticketNumber * 150;
+        }else {
+            total = ticketNumber * 200;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( CaseStudy2.calculateTicket(2, Category.SILVER));
+        System.out.println( CaseStudy2.calculateTicket(2, Category.GOLDEN));
+    }
 }
---
+++ src/com/casestudies/Category.java
@@ -0,0 +1,5 @@
+package com.casestudies;
+
+public enum Category {
+    SILVER, GOLDEN
+}
